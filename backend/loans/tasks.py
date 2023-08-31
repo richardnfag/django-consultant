e8c93b2f7d85ce4a3e448b39a3f121d3813e8403
Richard do Nascimento Fagundes <richardnfag@outlook.com>
2023-08-31 19:17:23 -0300

    fix: modify submit proposal task behavior

--- backend/loans/tasks.py
+++ backend/loans/tasks.py
@@ -11,15 +11,18 @@ def submit_proposal_task(proposal_id):
             "name": proposal.name,
         }
 
-        print(proposal_data)
         response = requests.post('https://loan-processor.digitalsys.com.br/api/v1/loan/', json=proposal_data)
 
         if response.status_code == 200:
             result = response.json().get('approved')
+   
             if result:
                 proposal.auto_approved = 'approved'
-                proposal.save()
-                return "Proposal updated with approval status"
+            else:
+                proposal.auto_approved = 'denied'
+
+            proposal.save()
+            return "Proposal updated with approval status"
 
         return "Error verifying proposal approval"
 
