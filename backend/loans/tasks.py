22e13b933e629c9c92938340700d508c40192f8e
Richard do Nascimento Fagundes <richardnfag@outlook.com>
2023-08-31 18:31:55 -0300

    fix: fix submit proposal task error

--- backend/loans/tasks.py
+++ backend/loans/tasks.py
@@ -11,12 +11,13 @@ def submit_proposal_task(proposal_id):
             "name": proposal.name,
         }
 
+        print(proposal_data)
         response = requests.post('https://loan-processor.digitalsys.com.br/api/v1/loan/', json=proposal_data)
 
         if response.status_code == 200:
             result = response.json().get('approved')
             if result:
-                proposal.approved = result
+                proposal.auto_approved = 'approved'
                 proposal.save()
                 return "Proposal updated with approval status"
 
